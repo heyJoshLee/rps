74b284d6e4ba374a78e02708528352f44c9a9b19
Josh Lee <Lee45276@gmail.com>
2015-06-12 21:22:01 +0800

    added input

--- rps.rb
+++ rps.rb
@@ -1,22 +1,14 @@
-CHOICES = {r: "Rock", p: "Paper", s: "Sciccors"}
+CHOICES = {"r" => "Rock", "p" => "Paper", "s" => "Sciccors"}
 
 puts "Welcome to Rock, Paper, Sciccors"
 
-def display_winning_msg()
-
-end
-
-
 loop do
-
   begin
-    puts "Please pick on (r, p, s)"
-    player.choice = gets.chomp.downcase
-  end until CHOICES.keys.include?(player _choices)
+    puts "Please pick one(r, p, s):"
+    player_choice = gets.chomp.downcase
+  end until CHOICES.keys.include?(player_choice)
 
-
-
-
-
-
+  puts "Your choice is => #{player_choice}"
 end
+
+
