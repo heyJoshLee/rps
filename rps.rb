29d421a90def2a8823d7010fbda251fdcc9e6a15
Josh Lee <Lee45276@gmail.com>
2015-06-12 21:53:50 +0800

    game works. would like to add score keeping later

--- rps.rb
+++ rps.rb
@@ -1,6 +1,17 @@
 CHOICES = {"r" => "Rock", "p" => "Paper", "s" => "Sciccors"}
-
 puts "Welcome to Rock, Paper, Sciccors"
+
+def check_win(player_choice, computer_choice,outcome)
+  msg = puts "Player picked #{CHOICES[player_choice]} and computer picked #{CHOICES[computer_choice]}"
+  case outcome
+  when "tie"
+    puts "You tied"
+  when "win"
+    puts "You win! :)"
+  when "lose"
+    puts "You lose :("
+  end
+end
 
 loop do
   begin
@@ -8,7 +19,37 @@ loop do
     player_choice = gets.chomp.downcase
   end until CHOICES.keys.include?(player_choice)
 
-  puts "Your choice is => #{player_choice}"
+  computer_choice = CHOICES.keys.sample
+
+  if player_choice == computer_choice
+    outcome = "tie" 
+    check_win(player_choice, computer_choice, outcome)
+  elsif player_choice == "r" && computer_choice == "p"
+    outcome = "lose"
+    check_win(player_choice, computer_choice, outcome)
+  elsif player_choice == "r" && computer_choice == "s"
+    outcome = "win"
+    check_win(player_choice, computer_choice, outcome)
+  elsif player_choice == "p" && computer_choice == "s"
+    outcome = "lose"
+    check_win(player_choice, computer_choice, outcome)
+  elsif player_choice == "p" && computer_choice == "r"
+    outcome = "win"
+    check_win(player_choice, computer_choice, outcome)
+  elsif player_choice == "s" && computer_choice == "r"
+    outcome = "lose"
+    check_win(player_choice, computer_choice, outcome)
+  elsif player_choice == "s" && computer_choice == "p"
+    outcome = "win"
+    check_win(player_choice, computer_choice, outcome)    
+  end
+
+  puts "Play again? (Y/N)"
+  still_play = gets.chomp.downcase
+  if still_play == "n"
+    puts "Thanks for playing!"
+    break
+  end
 end
 
 
